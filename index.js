fb8f4ff4a1c33ac84133e3e287e00f13d616b731
sharma242000 <sharmamohit242000@gmail.com>
2022-05-28 12:46:26 +0530

    newcommit

--- index.js
+++ index.js
@@ -1,19 +1,165 @@
 const express = require("express");
 const { google } = require("googleapis");
+const { composer } = require("googleapis/build/src/apis/composer");
+const https = require('https')
+var axios = require("axios").default;
+const qs = require('qs')
+var access_token
+var refresh_token=null;
+var codeid;
+var d;
+var err=false;
+
+// function redirect() {
+//   window.location.href = "http://localhost:1337";
+  
+// }
+
+
+
 
 const app = express();
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
-// app.use(express.static('../views/', {index: 'home.html'}))
+
 app.get("/addbill", (req, res) => {
   res.render("index");
   
 });
-app.get("/", (req, res) => {
-  res.render("home");
+
+var options = {
+  method: 'POST',
+  url: 'https://gymkhana.iitb.ac.in/profiles/oauth/token/',
+  headers: {'content-type': 'application/x-www-form-urlencoded'},
+  data: {
+    grant_type: 'authorization_code',
+    client_id: 'Rg3eTrYvWW3mgeadhDP4mVaGZ3mk7OUFsgiLFP3J',
+    client_secret: 'mVhKuyJpUYeqcwGeZdUM1pP53cM3RCEaKlWpcTXsp22lZZdvXjEAVUY271K4TXYmoWiR3zstfEmCSEc2Q5zC0fqMteET5829FNzyJWk7b8IYnQgjhry2zhxPQSf6AzXR',
+    code: 'KVTE1DJUbRMzxAcsg6z6z3GACLXM7A',
+    redirect_uri: 'http://localhost:1337/home'
+  }
+};
+
+// app.get("/home", (req, res) => {
+
+//   codeid=req.query.code
+//   var s;
+
+//   /////Token Exchange
+//   var options_post = {
+//     method: 'POST',
+//     url: 'https://gymkhana.iitb.ac.in/profiles/oauth/token/',
+//     headers: {'content-type': 'application/x-www-form-urlencoded'},
+//     data: qs.stringify({
+//       grant_type: 'authorization_code',
+//       client_id: 'Rg3eTrYvWW3mgeadhDP4mVaGZ3mk7OUFsgiLFP3J',
+//       client_secret: 'mVhKuyJpUYeqcwGeZdUM1pP53cM3RCEaKlWpcTXsp22lZZdvXjEAVUY271K4TXYmoWiR3zstfEmCSEc2Q5zC0fqMteET5829FNzyJWk7b8IYnQgjhry2zhxPQSf6AzXR',
+//       code: String(codeid),
+//       redirect_uri: 'http://localhost:1337/home'
+//     })
+//   };
+
+//   axios.request(options_post).then(function (response) {
+//     console.log(response.data);
+//     access_token=response.data.access_token;
+//     refresh_token=response.data.refresh_token;
+
+//   })
+//   .catch(function (error) {
+//     console.error(error.response.status);
+//   });
+//   res.render(home);
+
+// });
+
+app.get("/home",(req,res)=>{
+
+  codeid=req.query.code
+
+  var options_post = {
+    method: 'POST',
+    url: 'https://gymkhana.iitb.ac.in/profiles/oauth/token/',
+    headers: {'content-type': 'application/x-www-form-urlencoded'},
+    data: qs.stringify({
+      grant_type: 'authorization_code',
+      client_id: 'Rg3eTrYvWW3mgeadhDP4mVaGZ3mk7OUFsgiLFP3J',
+      client_secret: 'mVhKuyJpUYeqcwGeZdUM1pP53cM3RCEaKlWpcTXsp22lZZdvXjEAVUY271K4TXYmoWiR3zstfEmCSEc2Q5zC0fqMteET5829FNzyJWk7b8IYnQgjhry2zhxPQSf6AzXR',
+      code: String(codeid),
+      redirect_uri: 'http://localhost:1337/home'
+    })
+  };
+
+  var options_post1 = {
+    method: 'POST',
+    url: 'https://gymkhana.iitb.ac.in/profiles/oauth/token/',
+    headers: {'content-type': 'application/x-www-form-urlencoded'},
+    data: qs.stringify({
+      grant_type: 'refresh_token',
+      client_id: 'Rg3eTrYvWW3mgeadhDP4mVaGZ3mk7OUFsgiLFP3J',
+      client_secret: 'mVhKuyJpUYeqcwGeZdUM1pP53cM3RCEaKlWpcTXsp22lZZdvXjEAVUY271K4TXYmoWiR3zstfEmCSEc2Q5zC0fqMteET5829FNzyJWk7b8IYnQgjhry2zhxPQSf6AzXR',
+      refresh_token: String(refresh_token),
+      redirect_uri: 'http://localhost:1337/home'
+    })
+  };
   
+ Promise.all([
+    axios.request(options_post).then(function (response) {
+      console.log(response.data);
+      console.log("req1 working");
+      access_token=response.data.access_token;
+      refresh_token=response.data.refresh_token;
+  
+    })
+      .catch(function (error) {
+    console.error(error.response.status);
+  }),
+    axios.request(options_post1).then(function (response) {
+      console.log(response.data);
+      console.log("req2 working");
+
+      access_token=response.data.access_token;
+      refresh_token=response.data.refresh_token;
+  
+    })
+      .catch(function (error) {
+    console.error(error);
+    console.log("req2 not working");
+
+  })
+  ]);
+
+
+  res.render("home")
+
+
+
 });
+
+app.get("/", (req, res) => {
+  res.render("login"); 
+});
+
 app.get("/status", async (req, res) => {
+  
+
+  ////getting user data
+var options_get = {
+  method: 'GET',
+  url: 'https://gymkhana.iitb.ac.in/profiles/user/api/user/?fields=first_name,last_name,type,profile_picture,sex,username,email,program,contacts,insti_address,secondary_emails,mobile,roll_number',
+  headers: {'content-type': 'application/x-www-form-urlencoded','authorization':'Bearer ' + String(access_token)},
+ 
+};
+
+axios.request(options_get).then(function (response) {
+   d=Object.values(response.data)
+  console.log(d);  
+}).catch(function (error) {
+  console.error(error.response.status);
+  res.redirect('/')
+})
+
+
+
   const auth = new google.auth.GoogleAuth({
     keyFile: "readdata-346322-e567be4e5927.json",
     scopes: "https://www.googleapis.com/auth/spreadsheets",
@@ -39,16 +185,33 @@ app.get("/status", async (req, res) => {
     spreadsheetId,
     range: "Sheet1",
   });
- var data_array=getRows.data.values
-  console.log(data_array)
 
-  
-  res.render("status",{data:data_array});
+
+
+ var data_array=getRows.data.values
+
+  res.render("status",{data:data_array,d});
   
 });
 
 app.post("/addbill", async (req, res) => {
-  const { rollno, name,link } = req.body;
+
+  var options_get = {
+    method: 'GET',
+    url: 'https://gymkhana.iitb.ac.in/profiles/user/api/user/?fields=first_name,last_name,type,profile_picture,sex,username,email,program,contacts,insti_address,secondary_emails,mobile,roll_number',
+    headers: {'content-type': 'application/x-www-form-urlencoded','authorization':'Bearer ' + String(access_token)},
+   
+  };
+  
+  axios.request(options_get).then(function (response) {
+     d=Object.values(response.data)
+    console.log(d);  
+  }).catch(function (error) {
+    console.error(error.response.status);
+    res.redirect('/')
+  })
+
+  const { name,link } = req.body;
 
   const auth = new google.auth.GoogleAuth({
     keyFile: "readdata-346322-e567be4e5927.json",
@@ -83,11 +246,21 @@ app.post("/addbill", async (req, res) => {
     range: "Sheet1",
     valueInputOption: "USER_ENTERED",
     resource: {
-      values: [[rollno, name,link]],
+      values: [[d[1], name,link]],
     },
   });
 
   res.send("Successfully submitted! Thank you!");
 });
 
+
+
+app.get("/get", (req, res) => {
+
+  
+  
+});
+
 app.listen(1337, (req, res) => console.log("running on 1337"));
+
+
---
+++ new.js
@@ -0,0 +1,2 @@
+access_token='sdfkjbsjvhbk1'
+ console.log('Bearer '+ access_token)
